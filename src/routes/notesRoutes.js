3db6b93bdec534e18278115ccd22d57f4bca2896
Alina Y. <devadevam.tod@gmail.com>
2025-12-19 01:23:07 +0200

    finish

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -6,6 +6,13 @@ import {
   getNoteById,
   updateNote,
 } from '../controllers/notesController.js';
+import { celebrate } from 'celebrate';
+import {
+  createNoteSchema,
+  getAllNotesSchema,
+  noteIdSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
@@ -13,14 +20,14 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello!' });
 });
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
-router.post('/notes', createNote);
+router.post('/notes', celebrate(createNoteSchema), createNote);
 
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { errors } from 'celebrate';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 await connectMongoDB();
 app.listen(PORT, () => {
