fdc79dd4dc34927aff804228c47121a159a4375e
Alina Y. <devadevam.tod@gmail.com>
2025-12-23 01:25:52 +0200

    models, time constant

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minLength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('User', userSchema);
